bf03f3c0731934106b83b7e05acfaa162b9ee141
adampoewilson <adam62385@gmail.com>
2025-05-19 23:23:31 -0400

    Made the log give note names, too!

--- note_log.js
+++ note_log.js
@@ -4,19 +4,21 @@ const midiNotes = [];
 
 // Function to log a note
 
-function logNote(noteNum, velocity) {
+function logNote(noteNum, velocity, noteName) {
     const entry = {
     note: noteNum,
     velocity: velocity,
+    name: noteName,
     time: Date.now()
     }
 
     midiNotes.push(entry);
 
-    console.log(`Logged: Note ${noteNum}  at velocity ${velocity}`);
+    console.log(`Logged: Note ${noteNum}, ${noteName}  at velocity ${velocity}`);
 }
 
 // Simulate note input
-logNote(60, 100); // Middle C
-logNote(64, 120); // E
-logNote(67, 127); // G
+logNote(60,`middle C`, 100); // Middle C
+
+logNote(64,`E`, 120); // E
+logNote(67,`G`, 127); // G
